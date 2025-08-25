c53059099e42d5aa8cf645a5801aa9b329549fc0
dd060606 <dd06.developpement@gmail.com>
2025-08-25 10:12:47 +0200

    feat(webapp-auth): handle server unavailability with error messages and retry option

--- webapp/src/pages/auth/Auth.tsx
+++ webapp/src/pages/auth/Auth.tsx
@@ -1,11 +1,18 @@
+import { Button } from "@/components/ui/button";
 import ApiClient from "@/utils/api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router";
 
 export default function Auth() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
+  const [serverUnavailable, setServerUnavailable] = useState(false);
 
-  useEffect(() => {
+  useEffect(() => navigateToProperPage(), []);
+
+  const navigateToProperPage = () => {
+    setServerUnavailable(false);
     ApiClient.get("/auth/info")
       .then((response) => {
         // Check auth status
@@ -20,13 +27,20 @@ export default function Auth() {
       })
       .catch((err) => {
         console.error("Error while checking setup completion:", err);
-        navigate("/setup", { replace: true });
+        setServerUnavailable(true);
       });
-  }, [navigate]);
+  };
 
   return (
     <div className="bg-sidebar dark:bg-background flex min-h-screen w-full items-center justify-center">
-      <div className="border-t-primary border-primary/30 h-12 w-12 animate-spin rounded-full border-4" />
+      {serverUnavailable ? (
+        <div className="flex flex-col items-center gap-4 text-center">
+          <p>{t("errors.server-unavailable")}</p>
+          <Button onClick={navigateToProperPage}>{t("errors.retry")}</Button>
+        </div>
+      ) : (
+        <div className="border-t-primary border-primary/30 h-12 w-12 animate-spin rounded-full border-4" />
+      )}
     </div>
   );
 }
