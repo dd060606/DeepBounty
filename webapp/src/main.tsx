12e8485e5811f73bb82670a156b3974352710470
dd060606 <dd06.developpement@gmail.com>
2025-09-01 11:13:47 +0200

    feat(settings): init empty settings page

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -12,6 +12,7 @@ import Login from "./pages/auth/Login.tsx";
 import AuthSetup from "./pages/auth/AuthSetup.tsx";
 import LostPassword from "./pages/auth/LostPassword.tsx";
 import Targets from "./pages/Targets.tsx";
+import Settings from "./pages/Settings.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -28,6 +29,7 @@ createRoot(document.getElementById("root")!).render(
             <Route path="/alerts" element={<Alerts />} />
             <Route path="/modules" element={<Modules />} />
             <Route path="/targets" element={<Targets />} />
+            <Route path="/settings" element={<Settings />} />
           </Route>
         </Routes>
       </BrowserRouter>
